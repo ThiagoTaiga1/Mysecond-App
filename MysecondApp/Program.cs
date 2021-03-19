397195fd6cd8d6b632dffbe032c5faf68b4d1484
ThiagoTaiga1 <68219703+ThiagoTaiga1@users.noreply.github.com>
2021-03-19 19:03:54 -0300

    Add files via upload
    asp.net api which aims to know your age
    .

---
+++ MysecondApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MySecondApp
+{ 
+class Program
+{ 
+static void Main(string[] args)
+{
+//programa de calculadora ! 
+int numero1, numero2, resultado = 0;
+string operacao;
+
+Console.WriteLine("Digite o primeiro número: ");
+numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a operação: ");
+operacao = Console.ReadLine();
+
+Console.WriteLine("Digite o segundo numero: ");
+numero2 = int.Parse(Console.ReadLine());
+//fiz um switch para englobar as principais operações !
+switch(operacao)
+{
+case "+" :
+resultado = numero1 + numero2;
+break;
+case "-":
+resultado = numero1 - numero2;
+break;
+case "/":
+resultado = numero1 / numero2;
+break;
+case "*" :
+resultado = numero1 * numero2;
+break;
+default:
+Console.WriteLine("operaçao invalida!");
+break;
+
+}
+Console.WriteLine("{0} {1} {2} = {3}", numero1, operacao,numero2,resultado);
+Console.ReadKey(true);
+}
+}
+}
